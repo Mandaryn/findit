3883cf30c6bbdf4ec62e6cf4fe7763743848d96e
Piotr Marciniak <mandaryyyn@gmail.com>
2014-11-30 22:14:33 +0100

    basic distance calculations

--- client/findit.js
+++ client/findit.js
@@ -1,12 +1,21 @@
 // define basic places collection client side
 var places = new Meteor.Collection(null);
-places.insert({ name: "First place", image: 'place_1.jpeg' });
-places.insert({ name: "Second place", image: 'place_1.jpeg' });
-places.insert({ name: "Third place", image: 'place_1.jpeg' });
+places.insert({ name: "Close place", image: 'place_1.jpeg', latitude: 51.777401, longitude: 19.4123239 });
+places.insert({ name: "Farther place", image: 'place_1.jpeg', latitude: 51.775801, longitude: 19.4133239 });
+places.insert({ name: "Farthest place", image: 'place_1.jpeg', latitude: 51.772028, longitude: 19.414764 });
 
 // access places collection in view
 Template.body.helpers({
   places: places.find({}).fetch()
+});
+
+Template.place.helpers({
+  distance: function() {
+    if (!Geolocation.currentLocation()) {
+      return '???';
+    }
+    return geolib.getDistance(Geolocation.currentLocation().coords, this);
+  }
 });
 
 // display live current location

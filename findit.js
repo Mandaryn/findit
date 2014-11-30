9ce1e1fc7139a631e5218b05fc3084232b02318e
Piotr Marciniak <mandaryyyn@gmail.com>
2014-11-30 15:51:44 +0100

    use clientside collection to manage places in javascript

--- findit.js
+++ findit.js
@@ -1,14 +1,17 @@
 // simple-todos.js
 if (Meteor.isClient) {
-  // This code only runs on the client
+  // define basic places collection client side
+  var places = new Meteor.Collection(null);
+  places.insert({ name: "First place", image: 'place_1.jpeg' });
+  places.insert({ name: "Second place", image: 'place_1.jpeg' });
+  places.insert({ name: "Third place", image: 'place_1.jpeg' });
+
+  // access places collection in view
   Template.body.helpers({
-    places: [
-      { name: "First place", image: 'place_1.jpeg' },
-      { name: "Second place", image: 'place_1.jpeg' },
-      { name: "Third place", image: 'place_1.jpeg' }
-    ]
+    places: places.find({}).fetch()
   });
 
+  // add callback to geolocation api
   function getLocation() {
     if (navigator.geolocation) {
       navigator.geolocation.watchPosition(showPosition);
@@ -16,6 +19,7 @@ if (Meteor.isClient) {
       alert("Geolocation is not supported by this browser.");
     }
   };
+  // display geoposition in site heading
   function showPosition(position) {
     $('.latitude').html(position.coords.latitude);
     $('.longitude').html(position.coords.longitude);

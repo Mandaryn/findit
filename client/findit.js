752318a97432923d6a6376310f78dcec3a9690c4
Piotr Marciniak <mandaryyyn@gmail.com>
2014-12-02 00:40:23 +0100

    add right here place and added flex wrap

--- client/findit.js
+++ client/findit.js
@@ -3,6 +3,7 @@ var places = new Meteor.Collection(null);
 places.insert({ name: "Farthest place", image: 'place_1.jpeg', latitude: 51.772028, longitude: 19.414764 });
 places.insert({ name: "Farther place", image: 'place_1.jpeg', latitude: 51.775801, longitude: 19.4133239 });
 places.insert({ name: "Close place", image: 'place_1.jpeg', latitude: 51.777401, longitude: 19.4123239 });
+places.insert({ name: "Right here", image: 'place_1.jpeg', latitude: 51.7779007, longitude: 19.411222199999997 });
 
 // access places collection in view
 Template.body.helpers({

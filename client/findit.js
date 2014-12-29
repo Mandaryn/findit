a202f928c31654e15a63a7524af03dffbdfb152a
Mandaryn <mandaryn@ragnarson.com>
2014-12-29 20:22:06 +0100

    moved routes definitions to a separate file

--- client/findit.js
+++ client/findit.js
@@ -39,16 +39,3 @@ Template.place.helpers({
 Template.coordinates.helpers({
   currentCoordinates: Geolocation.latLng
 });
-
-Router.configure({
-  loadingTemplate: 'loading',
-  layoutTemplate: 'mainLayout'
-});
-
-Router.route('/', function () {
-  this.render('findit');
-});
-
-Router.route('/faq', function () {
-  this.render('faq');
-});
